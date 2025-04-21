a94406db928e6a63edf3d4a10a3494987d4a4057
oblivisheee <molniya213y@gmail.com>
2025-04-21 12:49:20 +0300

    server update, batch get

--- server/src/conf.rs
+++ server/src/conf.rs
@@ -23,26 +23,6 @@ impl Config {
         let config: Config = toml::from_str(&data)?;
         Ok(config)
     }
-    pub fn from_ron(path: &str) -> Result<Self, ConfigError> {
-        let data = match std::fs::read_to_string(path) {
-            Ok(data) => data,
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::NotFound {
-                    return Err(ConfigError::NotFound);
-                } else {
-                    return Err(ConfigError::Fs(e));
-                }
-            }
-        };
-        let config: Config = ron::from_str(&data)?;
-        Ok(config)
-    }
-    pub fn to_toml(&self) -> String {
-        toml::to_string_pretty(self).unwrap()
-    }
-    pub fn to_ron(&self) -> String {
-        ron::to_string(self).unwrap()
-    }
 }
 #[derive(thiserror::Error, Debug)]
 pub enum ConfigError {
@@ -50,8 +30,6 @@ pub enum ConfigError {
     Fs(#[from] std::io::Error),
     #[error("TOML parse error: {0}")]
     Toml(#[from] toml::de::Error),
-    #[error("RON parse error: {0}")]
-    Ron(#[from] ron::de::SpannedError),
     #[error("Config not found")]
     NotFound,
 }
